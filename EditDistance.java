38d53594d00b117d394a47ef57309847d2206cc6
Nishtha Maurya <108268302+nishtha9477@users.noreply.github.com>
2024-08-02 22:47:19 +0530

    Add files via upload

---
+++ EditDistance.java
@@ -0,0 +1,29 @@
+public class EditDistance {
+
+    public int editDistance(String str1, String str2) {
+    
+        int m=str1.length(), n=str2.length();
+        
+        int dp[][]= new int [m+1][n+1];
+        for(int i=0;i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==0){
+                    dp[i][j]=j;
+                }
+                else if(j==0){
+                    dp[i][j]=i;
+                }
+                else if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+ Math.min(dp[i-1][j],
+                                Math.min(dp[i][j-1],
+                                dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+}
